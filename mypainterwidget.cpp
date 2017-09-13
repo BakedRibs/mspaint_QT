ed27d133a38b3dc185cfd1081d3b2078075fe5e7
BakedRibs <18101325879@163.com>
2017-09-13 16:48:50 +0800

    version 0.17
    adding some functions

--- mypainterwidget.cpp
+++ mypainterwidget.cpp
@@ -7,6 +7,10 @@ MyPainterWidget::MyPainterWidget(QWidget *parent) : QWidget(parent)
     this->setMouseTracking(true);
     this->isPressed = false;
 
+    parametersTemp.paintColor = QColor(0,0,0);      //启动程序后，默认颜色为黑色
+    parametersTemp.paintWidth = 1;                  //启动程序后，默认画笔粗细为1
+    parametersTemp.paintType = pencil;                   //启动程序后，默认画笔类型为铅笔
+
     this->setAutoFillBackground(true);
     QPalette pal;
     pal.setColor(QPalette::Background,Qt::white);
@@ -17,21 +21,6 @@ MyPainterWidget::MyPainterWidget(QWidget *parent) : QWidget(parent)
 MyPainterWidget::~MyPainterWidget()
 {
 
-}
-
-void MyPainterWidget::paintEvent(QPaintEvent *p)
-{
-    QPainter painter(this);
-    QPen pen;
-    pen.setColor(Qt::black);
-    pen.setWidth(1);
-    painter.setPen(pen);
-
-    for(int i=0;i<lines.size();i++)
-    {
-        myLine *pLine = lines[i];
-        painter.drawLine(pLine->startPnt,pLine->endPnt);
-    }
 }
 
 void MyPainterWidget::mousePressEvent(QMouseEvent *e)
@@ -51,7 +40,7 @@ void MyPainterWidget::mouseMoveEvent(QMouseEvent *e)
         myLine *line = new myLine;
         line->startPnt = startPnt;
         line->endPnt = endPnt;
-        this->lines.push_back(line);
+        this->linesTemp.push_back(line);
 
         update();
         startPnt = endPnt;
@@ -62,4 +51,36 @@ void MyPainterWidget::mouseReleaseEvent(QMouseEvent *e)
 {
     setCursor(Qt::ArrowCursor);
     this->isPressed = false;
+
+    paintDetails *detailsTemp = new paintDetails;
+    detailsTemp->parameters = parametersTemp;
+    detailsTemp->paintLines = linesTemp;
+    this->currentPaintDetails.push_back(detailsTemp);
+}
+
+void MyPainterWidget::paintEvent(QPaintEvent *p)
+{
+    QPainter *painter = new QPainter(this);
+    QPen pen;
+    pen.setColor(parametersTemp.paintColor);
+    pen.setWidth(parametersTemp.paintWidth);
+    painter->setPen(pen);
+
+    switch (parametersTemp.paintType)
+    {
+        case pencil:
+            paintType_pencil(painter);
+            break;
+        default:
+            break;
+    }
+}
+
+void MyPainterWidget::paintType_pencil(QPainter *painterTemp)
+{
+    for(int i=0;i<linesTemp.size();i++)
+    {
+        myLine *pLine = linesTemp[i];
+        painterTemp->drawLine(pLine->startPnt,pLine->endPnt);
+    }
 }
--- mypainterwidget.h
+++ mypainterwidget.h
@@ -10,12 +10,52 @@
 #include <QPen>
 #include <QMouseEvent>
 #include <QPalette>
+#include <QColor>
 
-typedef struct myLine
+struct myLine                   //存放鼠标轨迹的线段序列
 {
-    QPoint startPnt;
-    QPoint endPnt;
-}myLine;
+    QPoint startPnt;            //起点
+    QPoint endPnt;              //终点
+};
+enum paintTypeEnum              //存放绘制类型参数
+{
+    pencil=1,                   //1     铅笔
+    fillWithColor,              //2     用颜色填充
+    text,                       //3     文本
+    eraser,                     //4     橡皮擦
+    colorSelect,                //5     颜色选取器
+    magnifier,                  //6     放大镜
+    line,                       //7     直线
+    curve,                      //8     曲线
+    ellipse,                    //9     椭圆
+    rectangle,                  //10    矩形
+    roundedRectangle,           //11    圆角矩形
+    triangle,                   //12    三角形
+    rightAngledTriangle,        //13    直角三角形
+    diamond,                    //14    菱形
+    pentagon,                   //15    五边形
+    hexagon,                    //16    六边形
+    arrow,                      //17    箭头
+    fourPointedStar,            //18    四角星
+    fivePointedStar,            //19    五角星
+    sixPointedStar,             //20    六角星
+    rectangleDialogBox,         //21    矩形对话框
+    roundDialogBox,             //22    圆形对话框
+    heart,                      //23    心形
+    flash                       //24    闪电
+};
+struct paintParameters          //存放绘制参数
+{
+    QColor paintColor;          //绘制颜色
+    qint32 paintWidth;          //绘制粗细
+    paintTypeEnum paintType;    //绘制类型
+};
+struct paintDetails
+{
+    paintParameters parameters; //绘制参数
+    QVector<myLine*> paintLines;          //线段序列
+};
+
 
 class MyPainterWidget : public QWidget
 {
@@ -29,11 +69,15 @@ public:
     void mouseMoveEvent(QMouseEvent *e);
     void mouseReleaseEvent(QMouseEvent *e);
 
-    QPoint startPnt;    //起点
-    QPoint endPnt;      //终点
-    bool isPressed;     //鼠标是否按下
+    QPoint startPnt;            //起点
+    QPoint endPnt;              //终点
+    bool isPressed;             //鼠标是否按下
 
-    QVector<myLine*> lines; //存放所有的线段
+    paintParameters parametersTemp;     //暂时存放本次拖动使用的参数
+    QVector<myLine*> linesTemp;         //暂时存放本次拖动形成的线段
+    QVector<paintDetails*> currentPaintDetails; //本次程序绘制的所有图案
+
+    void paintType_pencil(QPainter *painterTemp);
 };
 
 #endif // MYPAINTERWIDGET_H
