3c8970655b2eaaf98dba2299ed20079ff60b6993
BakedRibs <18101325879@163.com>
2017-09-21 18:12:04 +0800

    20170921-1811

--- penwidthfunctionwidget.h
+++ penwidthfunctionwidget.h
@@ -8,20 +8,20 @@
 
 class penwidthFunctionWidget : public QWidget
 {
-    Q_OBJECT        //注意：若包含自定义SIGNAL或SLOT，则需包含此行
+    Q_OBJECT                                    //注意：若包含自定义SIGNAL或SLOT，则需包含此行
 
 public:
     penwidthFunctionWidget(QWidget *parent);
     ~penwidthFunctionWidget();
 
-    QSpinBox *penWidthSpinBox;  //设置画笔宽度
+    QSpinBox *penWidthSpinBox;                  //设置画笔宽度
     QLabel *label;
 
 public slots:
-    void penWidthClicked(int value);
+    void penWidthClicked(int value);            //画笔宽度调整后事件
 
 signals:
-    void penWidthChanged(int changedValue);
+    void penWidthChanged(int changedValue);     //发送调整后宽度信号
 };
 
 #endif // PENWIDTHFUNCTIONWIDGET_H
